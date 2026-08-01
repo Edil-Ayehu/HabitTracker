72be485aded919aee2898ca35ef6b7019c010b19
edilayehu <edilayehu534027@gmail.com>
2026-08-01 12:01:11 +0300

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Edil on 01/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
